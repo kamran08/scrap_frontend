5e0679553666efdc3d1423c379c040329ee63ef0
itskamranweb <ahmedkamran265@gmail.com>
2021-05-12 01:22:28 +0600

    updated

--- pages/profile.vue
+++ pages/profile.vue
@@ -438,8 +438,10 @@
                 
                 
             >
-                
-                <Button icon="ios-cloud-upload-outline">Photo</Button>
+                 <div style="width: 70px;height:58px;line-height: 58px;">
+                                    <Icon type="ios-camera" size="20"></Icon>
+                                </div>
+                <!-- <Button icon="ios-cloud-upload-outline">Photo</Button> -->
         </Upload>
         </template>
         
@@ -689,3 +691,44 @@ export default {
     
 }
 </script>
+<style>
+    .demo-upload-list{
+        display: inline-block;
+        width: 60px;
+        height: 60px;
+        text-align: center;
+        line-height: 60px;
+        border: 1px solid transparent;
+        border-radius: 4px;
+        overflow: hidden;
+        background: #fff;
+        position: relative;
+        box-shadow: 0 1px 1px rgba(0,0,0,.2);
+        margin-right: 4px;
+    }
+    .demo-upload-list img{
+        width: 100%;
+        height: 100%;
+    }
+    .demo-upload-list-cover{
+        display: none;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: rgba(0,0,0,.6);
+    }
+    .demo-upload-list:hover .demo-upload-list-cover{
+        display: block;
+    }
+    .demo-upload-list-cover i{
+        color: #fff;
+        font-size: 20px;
+        cursor: pointer;
+        margin: 0 2px;
+    }
+    .ivu-upload{
+            width: 74px  !important; 
+    }
+</style>
